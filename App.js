f370963dca8a19ac75af82acc60697edb3014a2e
Dimas Sulz <dimassulz@gmail.com>
2018-10-07 15:49:20 -0300

    Finalizando aplicaçào adicionando notificacoes e finalizando botoes

--- App.js
+++ App.js
@@ -1,71 +1,13 @@
-import React from 'react';
-import {createBottomTabNavigator, createStackNavigator } from 'react-navigation'
-import ListDeck from './components/ListDeck';
-import NewDeck from './components/NewDeck';
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import React , {Component} from 'react';
 import { View } from 'react-native';
-import ViewDeck from "./components/ViewDeck";
 import StatusBarDeck from "./components/StatusBarDeck";
-import NewCard from "./components/NewCard";
-import StartQuiz from "./components/StartQuiz";
+import { setNotification } from './utils/notifications';
+import {NavigationStack} from './utils/NavigationStack'
 
-const MainTabs = createBottomTabNavigator({
-        List: {
-            screen: ListDeck,
-            navigationOptions: {
-                tabBarLabel: "Lista de Baralhos",
-                tabBarIcon: ({focused, tintColor}) => {
-                    return <Ionicons name="ios-albums-outline" size={25} color={tintColor}/>;
-                }
-
-            }
-        },
-        New: {
-            screen: NewDeck,
-            navigationOptions: {
-                tabBarLabel: "Criar Baralho",
-                tabBarIcon: ({focused, tintColor}) => {
-                    return <Ionicons name="ios-add-outline" size={25} color={tintColor}/>;
-                }
-            }
-        }
-    },
-    {
-        tabBarOptions: {
-            activeTintColor: 'red',
-            inactiveTintColor: '#aaa'
-        }
+export default class App extends Component {
+    componentDidMount() {
+        setNotification();
     }
-);
-
-const NavigationStack = createStackNavigator({
-    Home: {
-        screen: MainTabs,
-        navigationOptions: {
-            header: null
-        }
-    },
-    ViewDeck: {
-        screen: ViewDeck,
-        navigationOptions: ({ navigation }) => ({
-            title: `Baralho - ${navigation.state.params.deck.title}`
-        })
-    },
-    NewCard: {
-        screen: NewCard,
-        navigationOptions: ({ navigation }) => ({
-            title: `${navigation.state.params.title} - Adicionar Pergunta`
-        })
-    },
-    Quiz: {
-        screen: StartQuiz,
-        navigationOptions: {
-            title: 'Quiz'
-        }
-    }
-});
-
-export default class App extends React.Component {
     render() {
         return (<View style={{ flex: 1 }}>
             <StatusBarDeck backgroundColor="red" barStyle="light-content" />
